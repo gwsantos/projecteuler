0d10f05a510304e65e0596ed771cdf005ad8e795
Gustavo De Souza Santos <42616195+gwsantos@users.noreply.github.com>
2019-12-08 22:35:23 -0500

    Added prime array for check. Correct answer for Problem10.

--- src/projetEuler/Problem10.java
+++ src/projetEuler/Problem10.java
@@ -1,36 +1,45 @@
 package projetEuler;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class Problem10 {
 
-	//Sum of all prime numbers below int N
+	// Sum of all prime numbers below int N
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
 		int N = 2000000;
-		int sum = 0;
-		for (int i = 1; i < N; i++) {
-			//System.out.println("Checagem do numero " + i);
-			if (isPrime(i)) {
-				sum += i;
-				//System.out.println("O numero " + i + " eh primo");
-			} //else {System.out.println("O numero " + i + " nao eh primo");}
+		BigInteger sum = BigInteger.TWO;
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		int firstPrime = 2; // 2 is the first prime number by definition
+		System.out.println("O numero " + firstPrime + " eh primo");
+		primes.add(firstPrime); 
+		for (int i = firstPrime; i < N; i++) {
+			// System.out.println("Checagem do numero " + i);
+			if (isPrime(i, primes)) {
+				sum = sum.add(BigInteger.valueOf(i));
+				primes.add(i);
+				 System.out.println("O numero " + i + " eh primo");
+			}  //else {System.out.println("O numero " + i + " nao eh primo");}
 		}
-		System.out.println("Soma: " + sum);
+		System.out.println("Soma: " + sum);	
+		long end = System.currentTimeMillis();
+		double executionTime = end-start;
+		System.out.println("Tempo de execução = " + executionTime);
 	}
 
-	private static boolean isPrime(int i) {
-		int divisores = 0;
-		if (i % 2 == 0 && i > 2) {
-			return false;
+	private static boolean isPrime(int i, ArrayList<Integer> primeArray) {
+		boolean isPrime = true;
+		for (int prime : primeArray) {
+			if (i % prime == 0) {
+				isPrime = false;
+				break;
+			} 				
 		}
-		for (int j = 1; j <= i/2 ; j++) {
-			if (divisores > 2) {
-				return false;
-			}
-			if (i%j==0) {
-				divisores++;
-				//System.out.println("O numero " +j+ " eh um divisor de "+i);
-			}			
-		}
-		return divisores == 2;
+		return isPrime;
 	}
+
+	
 
 }
